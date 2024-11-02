77357eb5b3487d70d569be04714b6c636bfafbaf
Anurag8853 <whyanurag8853@gmail.com>
2024-11-02 11:03:29 +0530

    first commit

---
+++ Program1.java
@@ -0,0 +1,17 @@
+class Program1
+{
+
+	public static void main(String[] args)
+	{
+		System.out.println("This is Program1");
+			
+		byte x=100;
+		System.out.println(x);
+		char y='@';
+		System.out.println(y);
+		System.out.println("Progam Ends");
+
+
+	}
+
+}
